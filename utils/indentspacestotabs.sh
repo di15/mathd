83dd117b522cecdd9771d35cab18fbd9372f3e15
nyanpasu <nyanpasugit@gmail.com>
2014-06-24 19:50:25 +0800

    Change script to reference ../ instead of ./ (As part of aa8a813)

--- utils/indentspacestotabs.sh
+++ utils/indentspacestotabs.sh
@@ -1,9 +1,9 @@
-find ./ -iname "*.cpp" | \
+find ../ -iname "*.cpp" | \
 	while read I; do
 		python indentspacestotabs.py -n 4 -i "$I"
 	done
 
-find ./ -iname "*.h" | \
+find ../ -iname "*.h" | \
 	while read I; do
 		python indentspacestotabs.py -n 4 -i "$I"
 	done
